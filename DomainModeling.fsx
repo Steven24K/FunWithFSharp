ba56684636f03bae36c233702be0fe388287f3c5
Steven24K <s.koerts2@gmail.com>
2026-04-02 12:24:33 +0200

    Optimize Customer a bit

--- DomainModeling.fsx
+++ DomainModeling.fsx
@@ -18,24 +18,67 @@ type Customer =
     | Eligible of Id:string
     | Registered of Id: string
     | Guest of Id: string
+    with
+        member this.Discount =
+            match this with
+            | Eligible _ -> 0.1M
+            | _ -> 0.0M
+
+
+type ValidationError =
+    | InputOutOfRange of string
+
+type Spend = private Spend of decimal
+    with
+        member this.Value = this |> fun (Spend value) -> value
+        static member Create input =
+            if input >= 0.0M && input <= 1000.0M then
+                Ok (Spend input)
+            else
+                Error (InputOutOfRange "You can only spend between 0 and 1000")
+
+type Total = decimal
+type CalculateTotal = Customer -> Spend -> Total
+
 
 let fred = Eligible "Fred"
 let john = Registered "John"
 let sarah = Guest "Sarah"
 
-let calculateTotal customer spend: decimal =
-    let discount = 
-        match customer with 
-        | Eligible _ when spend >= 100.0M -> spend * 0.1M
-        // | Registered _ -> 0.0M
-        // | Guest _ -> 0.0M
-        | _ -> 0.0M
-    spend - discount
+let calculateTotal (customer:Customer) (spend:Spend) =
+    let discount =
+        if spend.Value >= 100.0M then spend.Value * customer.Discount
+        else 0.0M
+    spend.Value - discount
 
-let assertFred = calculateTotal fred 100.0M = 90.0M
-let assertJohn = calculateTotal john 99.0M = 99.0M
-let assertSarah = calculateTotal sarah 100.0M = 100.0M
 
-printfn $"Fred: {assertFred}"
-printfn $"John: {assertJohn}"
-printfn $"Sarah: {assertSarah}"
+
+
+let isEqualTo expected actual = 
+    actual =expected
+
+let assertFred1 = calculateTotal fred (Spend 100.0M) |> isEqualTo 90.0M
+let assertJohn1 = calculateTotal john (Spend 99.0M) |> isEqualTo 99.0M
+let assertSarah1 = calculateTotal sarah (Spend 100.0M) |> isEqualTo 100.0M
+
+printfn $"Fred: {assertFred1}"
+printfn $"John: {assertJohn1}"
+printfn $"Sarah: {assertSarah1}"
+
+let assertEqual customer spent expected =
+    Spend.Create spent
+    |> Result.map (fun spend -> calculateTotal customer spend)
+    |> isEqualTo (Ok expected)
+
+let assertJohn2 = assertEqual john 100.0M 90.0M
+let assertSarah2 = assertEqual sarah 100.0M 100.0M
+
+type Latitude = decimal
+type Longitude = decimal
+
+type GpsCoordinate = { Latitude: Latitude; Longitude: Longitude }
+
+let badGps : GpsCoordinate = { Latitude = 1000M; Longitude = -345M }
+let latitude = 46M
+let longitude = 15M
+let badGps2 : GpsCoordinate = { Latitude = longitude; Longitude = latitude }
